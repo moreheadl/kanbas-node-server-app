6191167e85fa4536cbcb1f1e6282cb3c857283ff
moreheadl <morehead.l@northeastern.edu>
2024-04-01 14:15:05 -0400

    first commit

---
+++ Kanbas/Database/index.js
@@ -0,0 +1,6 @@
+import courses from "./courses.js";
+import modules from "./modules.js";
+import assignments from "./assignments.js";
+import statuses from "./statuses.js";
+
+export default { courses, modules, assignments, statuses };
