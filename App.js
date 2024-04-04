577c1c5898fcd1dc594cfe5fc14f9ba3130906a9
moreheadl <morehead.l@northeastern.edu>
2024-04-03 20:00:04 -0400

    hello

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
+import Hello from "./Hello.js";
 
 const app = express();
 app.use(cors());
@@ -10,4 +11,5 @@ app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
+Hello(app);
 app.listen(process.env.PORT || 4000);
