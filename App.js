f3c13801c9d8cea0404822cb5407e05cee29dd49
moreheadl <morehead.l@northeastern.edu>
2024-04-15 04:29:17 -0400

    lab mongo setup

--- App.js
+++ App.js
@@ -4,10 +4,15 @@ import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import Hello from "./Hello.js";
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
 
+
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
